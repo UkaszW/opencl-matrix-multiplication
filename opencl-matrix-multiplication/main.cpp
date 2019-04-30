81d0274e288c4b2779f69f8f40c82e0f3e116b7a
Lukasz Wojcik <lukasz.wojcik.96@gmail.com>
2019-04-30 08:59:50 +0200

    Initial Commit

---
+++ opencl-matrix-multiplication/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opencl-matrix-multiplication
+//
+//  Created by Łukasz Wójcik on 30/04/2019.
+//  Copyright © 2019 Łukasz Wójcik. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
